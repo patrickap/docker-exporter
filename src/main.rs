a039a5c2d15708d2b9d09b7a1badfffb55fc0466
patrickap <patrick.schlageter@web.de>
2024-02-23 21:43:51 +0100

    feat: plain async tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,40 @@
-use std::{error::Error, sync::Arc};
+use std::time::Duration;
 
-use tiny_http;
-use tokio;
+use async_std::{io, net, prelude::*, task};
 
 mod config;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-  let server = Arc::new(tiny_http::Server::http(config::SERVER_ADDRESS)?);
-  let server_ref = Arc::clone(&server);
-  println!("server listening on {}", config::SERVER_ADDRESS);
+#[async_std::main]
+async fn main() -> io::Result<()> {
+  let server = net::TcpListener::bind(config::SERVER_ADDRESS).await?;
+  println!("server listening on {}", server.local_addr()?);
 
-  // TODO: without move possible?
-  let signals = tokio::spawn(signals(move || server_ref.unblock()));
+  while let Some(stream) = server.incoming().next().await {
+    task::spawn(async move {
+      return handle_connection(stream?).await;
+    });
+  }
 
-  let mut tasks = tokio::task::JoinSet::new();
-  let tasks = tokio::spawn(async move {
-    for request in server.incoming_requests() {
-      tasks.spawn_blocking(|| {
-        handle_request(request);
-      });
-    }
-  });
-
-  tokio::try_join!(signals, tasks)?;
-  println!("shutting down server");
   Ok(())
 }
 
-fn handle_request(request: tiny_http::Request) {
-  let result = match request.url() {
-    "/metrics" => request.respond(tiny_http::Response::from_string(
-      "metrics can be viewed here",
-    )),
-    _ => request.respond(tiny_http::Response::from_string("404")),
+async fn handle_connection(mut stream: net::TcpStream) -> io::Result<()> {
+  task::sleep(Duration::from_millis(5000)).await;
+
+  let buffer_reader = io::BufReader::new(&stream);
+  let request_line = buffer_reader.lines().next().await.unwrap()?;
+
+  let (status, response) = if request_line == "GET / HTTP/1.1" {
+    ("HTTP/1.1 200 OK", "hello world")
+  } else {
+    ("HTTP/1.1 404 NOT FOUND", "404")
   };
 
-  if let Err(e) = result {
-    eprintln!("failed to handle request: {e}")
-  }
-}
+  let length = response.len();
 
-#[cfg(unix)]
-async fn signals(cleanup: impl FnOnce() -> ()) {
-  // TODO: handle unwrap()
-  let mut sigint =
-    tokio::signal::unix::signal(tokio::signal::unix::SignalKind::interrupt()).unwrap();
-  let mut sigterm =
-    tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate()).unwrap();
+  let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{response}");
 
-  tokio::select! {
-    _ = sigint.recv() => {
-      println!("received SIGINT");
-      cleanup()
-    },
-    _ = sigterm.recv() => {
-      println!("received SIGTERM");
-      cleanup()
-    },
-  }
-}
+  stream.write_all(response.as_bytes()).await?;
 
-#[cfg(windows)]
-async fn signals(cleanup: impl FnOnce() -> ()) {
-  // TODO: handle windows signals
+  Ok(())
 }
