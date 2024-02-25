8682754635f0d11822459376b2a295a886bcba2a
patrickap <patrick.schlageter@web.de>
2024-02-25 19:54:57 +0100

    chore: imports

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use actix_web;
-use std;
+use actix_web as actix;
+use std::{io, net};
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-  let address = std::net::SocketAddrV4::new(std::net::Ipv4Addr::new(0, 0, 0, 0), 9630);
-  let server = actix_web::HttpServer::new(|| actix_web::App::new().service(metrics))
+#[actix::main]
+async fn main() -> io::Result<()> {
+  let address = net::SocketAddrV4::new(net::Ipv4Addr::new(0, 0, 0, 0), 9630);
+  let server = actix::HttpServer::new(|| actix::App::new().service(metrics))
     .bind(address)?
     .workers(2)
     .run();
@@ -16,11 +16,11 @@ async fn main() -> std::io::Result<()> {
   Ok(())
 }
 
-#[actix_web::get("/metrics")]
-async fn metrics() -> Result<impl actix_web::Responder, actix_web::Error> {
+#[actix::get("/metrics")]
+async fn metrics() -> Result<impl actix::Responder, actix::Error> {
   Ok(
-    actix_web::HttpResponse::build(actix_web::http::StatusCode::OK)
-      .content_type(actix_web::http::header::ContentType::plaintext())
+    actix::HttpResponse::build(actix::http::StatusCode::OK)
+      .content_type(actix::http::header::ContentType::plaintext())
       .body("Hello World"),
   )
 }
