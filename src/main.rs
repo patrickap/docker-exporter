ddad3668a07a9ed2bddaf15b2e7f365562a2c603
patrickap <patrick.schlageter@web.de>
2024-02-28 11:24:51 +0100

    chore: log server address

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ async fn main() -> std::io::Result<()> {
   let listener = tokio::net::TcpListener::bind("0.0.0.0:9630").await?;
   let router = axum::Router::new().route("/metrics", axum::routing::get(metrics));
 
-  println!("server listening on {:?}", listener.local_addr());
+  println!("server listening on {:?}", listener.local_addr()?);
   axum::serve(listener, router).await?;
 
   println!("server stopped");
