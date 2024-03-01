5944fa3b1b6ac198769786599a1e9b4a29ebb279
patrickap <patrick.schlageter@web.de>
2024-03-01 13:36:02 +0100

    fix: remove registry mutex

--- src/config/route.rs
+++ src/config/route.rs
@@ -1,14 +1,14 @@
 use axum::Extension;
 use prometheus_client::{encoding::text, registry::Registry};
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 pub async fn status() -> &'static str {
   "ok"
 }
 
-pub async fn metrics(axum::Extension(registry): Extension<Arc<Mutex<Registry>>>) -> String {
+pub async fn metrics(axum::Extension(registry): Extension<Arc<Registry>>) -> String {
   // TODO: do not unwrap
   let mut encoded = String::new();
-  text::encode(&mut encoded, &registry.lock().unwrap()).unwrap();
+  text::encode(&mut encoded, &registry).unwrap();
   encoded
 }
--- src/main.rs
+++ src/main.rs
@@ -15,20 +15,17 @@ use crate::config::{registry, route, server};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-  let registry = Arc::new(Mutex::new(Registry::with_prefix(registry::PREFIX)));
+  let mut registry = Registry::with_prefix(registry::PREFIX);
   // TODO: do not unwrap, use http socket
-  registry
-    .lock()
-    .unwrap()
-    .register_collector(Box::new(DockerCollector::new(
-      Docker::connect_with_socket_defaults().unwrap(),
-    )));
+  registry.register_collector(Box::new(DockerCollector::new(
+    Docker::connect_with_socket_defaults().unwrap(),
+  )));
 
   let listener = TcpListener::bind(server::ADDRESS).await?;
   let router = Router::new()
     .route("/status", routing::get(route::status))
     .route("/metrics", routing::get(route::metrics))
-    .layer(axum::Extension(registry));
+    .layer(axum::Extension(Arc::new(registry)));
 
   println!("server listening on {}", listener.local_addr()?);
   axum::serve(listener, router)
