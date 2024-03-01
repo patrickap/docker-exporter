e76f568f2d54a5fda0281534c2d9641754872212
patrickap <patrick.schlageter@web.de>
2024-03-01 23:46:35 +0100

    feat: almost there...

--- src/collector.rs
+++ src/collector.rs
@@ -14,8 +14,12 @@ pub trait Collector<S> {
   fn new() -> Self;
   fn collect(
     &self,
-    source: &S,
-    channel: &(mpsc::Sender<Self::Metric>, mpsc::Receiver<Self::Metric>),
+    source: S,
+    // TODO: makes this param and Arc<_> type sense when there can only be one receiver?
+    channel: (
+      Arc<mpsc::Sender<Self::Metric>>,
+      Arc<mpsc::Receiver<Self::Metric>>,
+    ),
   ) -> ();
 }
 
@@ -42,10 +46,13 @@ impl Collector<Docker> for DockerCollector {
 
   fn collect(
     &self,
-    docker: &Docker,
-    channel: &(mpsc::Sender<Self::Metric>, mpsc::Receiver<Self::Metric>),
+    docker: Docker,
+    channel: (
+      Arc<mpsc::Sender<Self::Metric>>,
+      Arc<mpsc::Receiver<Self::Metric>>,
+    ),
   ) {
-    tokio::spawn(async {
+    tokio::spawn(async move {
       let containers = docker
         .list_containers(Some(container::ListContainersOptions::<&str> {
           all: true,
@@ -55,36 +62,39 @@ impl Collector<Docker> for DockerCollector {
         .unwrap_or_default();
 
       for container in containers {
-        tokio::spawn(async {
-          let running = container.state.eq(&Some(String::from("running")));
+        let running = container.state.eq(&Some(String::from("running")));
 
-          let name = Arc::new(
-            container
-              .names
-              .and_then(|names| Some(names.join(";")))
-              .map(|names| names[1..].to_string())
-              .unwrap_or_default(),
-          );
+        let name = Arc::new(
+          container
+            .names
+            .and_then(|names| Some(names.join(";")))
+            .map(|names| names[1..].to_string())
+            .unwrap_or_default(),
+        );
 
-          // TODO: do not unwrap, should not await here to not block the loop
-          let stats = Arc::new(
-            docker
-              .stats(&container.id.unwrap_or_default(), Default::default())
-              .take(1)
-              .next()
-              .await
-              .unwrap()
-              .unwrap(),
-          );
+        let stats = Arc::new(
+          docker
+            .stats(&container.id.unwrap_or_default(), Default::default())
+            .take(1)
+            .next()
+            .await
+            .unwrap()
+            .unwrap(),
+        );
 
-          let (tx, _) = channel;
+        let (ref tx, _) = channel;
 
-          tokio::spawn(async {
-            let metric =
-              DockerCollector::new_state_metric(running, Arc::clone(&name), Arc::clone(&stats));
-            tx.send(metric)
-          });
+        let tx = Arc::clone(&tx);
+        tokio::spawn(async move {
+          let metric =
+            DockerCollector::new_state_metric(running, Arc::clone(&name), Arc::clone(&stats));
+          // TODO: do not unwrap
+          tx.send(metric).await.unwrap();
         });
+
+        if running {
+          // TODO: collect other metrics
+        }
       }
     });
   }
@@ -121,12 +131,11 @@ impl collector::Collector for DockerCollector {
     // TODO: comment why this is all needed, there is no async implementation so we need to execute this blocking
     // TODO: do not unwrap
     let docker = Docker::connect_with_socket_defaults().unwrap();
-    let channel = mpsc::channel::<DockerMetric>(32);
-    self.collect(&docker, &channel);
+    let (tx, rx) = mpsc::channel::<DockerMetric>(32);
+    self.collect(docker, (Arc::new(tx), Arc::new(rx)));
 
     // spawn local as DescriptorEncoder is not thread safe
     tokio::task::spawn_local(async {
-      let (_, rx) = channel;
       while let Some(metric) = rx.recv().await {
         // TODO: do not unwrap
         let metric_encoder = encoder
