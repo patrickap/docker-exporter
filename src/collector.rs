f96a2de1e5f3a106171edb0ce3d333199c8253d5
patrickap <patrick.schlageter@web.de>
2024-03-02 23:12:42 +0100

    feat: almost there

--- src/collector.rs
+++ src/collector.rs
@@ -1,26 +1,18 @@
 use bollard::{container, Docker};
-use futures::StreamExt;
+use futures::{future, StreamExt, TryFutureExt};
 use prometheus_client::{
   collector, encoding,
   metrics::{family, gauge},
   registry,
 };
 use std::sync::{Arc, Mutex};
-use tokio::sync::mpsc;
+use tokio::{sync::mpsc, task};
 
 pub trait Collector<S> {
   type Metric;
 
   fn new() -> Self;
-  fn collect(
-    &self,
-    source: S,
-    // TODO: makes this param and Arc<_> type sense when there can only be one receiver?
-    channel: (
-      Arc<mpsc::Sender<Self::Metric>>,
-      Arc<Mutex<mpsc::Receiver<Self::Metric>>>,
-    ),
-  ) -> ();
+  async fn collect(&self, source: Arc<S>) -> Vec<Result<Self::Metric, task::JoinError>>;
 }
 
 pub struct DockerMetric {
@@ -44,65 +36,58 @@ impl Collector<Docker> for DockerCollector {
     Self {}
   }
 
-  fn collect(
-    &self,
-    docker: Docker,
-    channel: (
-      Arc<mpsc::Sender<Self::Metric>>,
-      Arc<Mutex<mpsc::Receiver<Self::Metric>>>,
-    ),
-  ) {
-    let docker_p = Arc::new(docker);
-    tokio::spawn(async move {
-      let containers = Arc::clone(&docker_p)
-        .list_containers(Some(container::ListContainersOptions::<&str> {
-          all: true,
-          ..Default::default()
-        }))
-        .await
-        .unwrap_or_default();
+  async fn collect(&self, docker: Arc<Docker>) -> Vec<Result<Self::Metric, task::JoinError>> {
+    let containers = docker
+      .list_containers(Some(container::ListContainersOptions::<&str> {
+        all: true,
+        ..Default::default()
+      }))
+      .await
+      .unwrap_or_default();
 
-      for container in containers {
-        let docker_p_p = Arc::clone(&docker_p);
+    let mut tasks = Vec::new();
+    for container in containers {
+      let docker = Arc::clone(&docker);
 
-        // TODO: better way without using ref...?
-        let (ref tx, _) = channel;
-        let tx = Arc::clone(&tx);
+      tasks.push(task::spawn(async move {
+        let running = container.state.eq(&Some(String::from("running")));
 
-        tokio::spawn(async move {
-          let running = container.state.eq(&Some(String::from("running")));
+        let name = Arc::new(
+          container
+            .names
+            .and_then(|names| Some(names.join(";")))
+            .map(|names| names[1..].to_string())
+            .unwrap_or_default(),
+        );
 
-          let name = Arc::new(
-            container
-              .names
-              .and_then(|names| Some(names.join(";")))
-              .map(|names| names[1..].to_string())
-              .unwrap_or_default(),
-          );
+        // TODO: do not unwrap
+        let stats = Arc::new(
+          docker
+            .stats(&container.id.unwrap_or_default(), Default::default())
+            .take(1)
+            .next()
+            .await
+            .unwrap()
+            .unwrap(),
+        );
 
-          let stats = Arc::new(
-            Arc::clone(&docker_p_p)
-              .stats(&container.id.unwrap_or_default(), Default::default())
-              .take(1)
-              .next()
-              .await
-              .unwrap()
-              .unwrap(),
-          );
+        let metrics = Vec::from([task::spawn(async move {
+          DockerCollector::new_state_metric(running, Arc::clone(&name), Arc::clone(&stats))
+        })]);
 
-          tokio::spawn(async move {
-            let metric =
-              DockerCollector::new_state_metric(running, Arc::clone(&name), Arc::clone(&stats));
-            // TODO: do not unwrap
-            tx.send(metric).await.unwrap();
-          });
+        // TODO: add missing metrics
 
-          if running {
-            // TODO: collect other metrics
-          }
-        });
-      }
-    });
+        future::join_all(metrics).await
+      }));
+    }
+    let result = future::join_all(tasks)
+      .await
+      .into_iter()
+      .map(|r| r.unwrap_or_default())
+      .flatten()
+      .collect();
+
+    result
   }
 }
 
@@ -134,52 +119,26 @@ impl DockerCollector {
 
 impl collector::Collector for DockerCollector {
   fn encode(&self, mut encoder: encoding::DescriptorEncoder) -> Result<(), std::fmt::Error> {
-    // TODO: comment why this is all needed, there is no async implementation so we need to execute this blocking
-    // TODO: do not unwrap
-    let docker = Docker::connect_with_socket_defaults().unwrap();
-    let (tx, rx) = mpsc::channel::<DockerMetric>(32);
-    let (tx, rx) = (Arc::new(tx), Arc::new(Mutex::new(rx)));
+    task::block_in_place(|| async {
+      // TODO: do not unwrap
+      let docker = Arc::new(Docker::connect_with_socket_defaults().unwrap());
+      let metrics = self.collect(docker).await;
 
-    // TODO: better way than rx_p here?
-    let rx_p = Arc::clone(&rx);
+      for metric in metrics {
+        if let Ok(metric) = metric {
+          // TODO: do not unwrap
+          let metric_encoder = encoder
+            .encode_descriptor(
+              &metric.name,
+              &metric.help,
+              None,
+              metric.metric.metric_type(),
+            )
+            .unwrap();
 
-    // // spawn local as DescriptorEncoder is not thread safe
-    // tokio::task::spawn_local(async move {
-    //   // TODO: do not unwrap and lock unsafe
-    //   while let Some(metric) = rx_p.lock().unwrap().recv().await {
-    //     // TODO: do not unwrap
-    //     let metric_encoder = encoder
-    //       .encode_descriptor(
-    //         &metric.name,
-    //         &metric.help,
-    //         None,
-    //         metric.metric.metric_type(),
-    //       )
-    //       .unwrap();
-
-    //     // TODO: do not unwrap
-    //     metric.metric.encode(metric_encoder).unwrap();
-    //   }
-    // });
-
-    self.collect(docker, (tx, rx));
-
-    // TODO: block_in_place works but better get spawn_local above to work
-    tokio::task::block_in_place(move || {
-      // TODO: do not unwrap and lock unsafe
-      while let Some(metric) = rx_p.lock().unwrap().blocking_recv() {
-        // TODO: do not unwrap
-        let metric_encoder = encoder
-          .encode_descriptor(
-            &metric.name,
-            &metric.help,
-            None,
-            metric.metric.metric_type(),
-          )
-          .unwrap();
-
-        // TODO: do not unwrap
-        metric.metric.encode(metric_encoder).unwrap();
+          // TODO: do not unwrap
+          metric.metric.encode(metric_encoder).unwrap();
+        }
       }
     });
 
