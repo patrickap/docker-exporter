ab4f7c3dcca6b015eb83d30d26d72098b2ccc29c
patrickap <patrick.schlageter@web.de>
2024-03-05 13:31:10 +0100

    refactor: simplified code and switched to channels

--- src/collector.rs
+++ src/collector.rs
@@ -6,13 +6,13 @@ use prometheus_client::{
   registry,
 };
 use std::sync::{atomic::AtomicU64, Arc};
-use tokio::{runtime::Handle, task};
+use tokio::{runtime::Handle, sync::mpsc, task};
 
 pub trait Collector<S> {
   type Metric;
 
   fn new() -> Self;
-  async fn collect(&self, source: Arc<S>) -> Vec<Self::Metric>;
+  async fn collect(&self, source: Arc<S>, tx: Arc<mpsc::Sender<Self::Metric>>);
 }
 
 pub struct DockerMetric {
@@ -37,7 +37,7 @@ impl Collector<Docker> for DockerCollector {
     Self {}
   }
 
-  async fn collect(&self, docker: Arc<Docker>) -> Vec<Self::Metric> {
+  async fn collect(&self, docker: Arc<Docker>, tx: Arc<mpsc::Sender<Self::Metric>>) {
     let containers = docker
       .list_containers(Some(container::ListContainersOptions::<&str> {
         all: true,
@@ -46,119 +46,125 @@ impl Collector<Docker> for DockerCollector {
       .await
       .unwrap_or_default();
 
-    let tasks: Vec<_> = containers
-      .into_iter()
-      .map(|container| {
-        let docker = Arc::clone(&docker);
+    for container in containers {
+      let docker = Arc::clone(&docker);
+      let tx = Arc::clone(&tx);
 
-        task::spawn(async move {
-          let running = container.state.eq(&Some(String::from("running")));
+      task::spawn(async move {
+        let running = container.state.eq(&Some(String::from("running")));
 
-          let name = Arc::new(
-            container
-              .names
-              .and_then(|names| Some(names.join(";")))
-              .and_then(|mut name| Some(name.drain(1..).collect())),
-          );
+        let name = Arc::new(
+          container
+            .names
+            .and_then(|names| Some(names.join(";")))
+            .and_then(|mut name| Some(name.drain(1..).collect())),
+        );
 
-          let stats = Arc::new(
-            docker
-              .stats(
-                &container.id.unwrap_or_default(),
-                Some(container::StatsOptions {
-                  stream: false,
-                  ..Default::default()
-                }),
-              )
-              .take(1)
-              .next()
-              .map(|stats| match stats {
-                Some(Ok(stats)) => Some(stats),
-                _ => None,
-              })
-              .await,
-          );
-
-          let metrics = [
-            Self::new_state_metric,
-            Self::new_cpu_metric,
-            Self::new_memory_metric,
-            Self::new_io_metric,
-            Self::new_network_metric,
-          ]
-          .map(|metric| {
-            let name = Arc::clone(&name);
-            let stats = Arc::clone(&stats);
-
-            task::spawn(async move { metric(name, stats, running) })
-          });
-
-          future::join_all(metrics)
-            .await
-            .into_iter()
-            .flat_map(|metric| match metric {
-              Ok(Some(metric)) => [metric],
-              _ => Default::default(),
+        let stats = Arc::new(
+          docker
+            .stats(
+              &container.id.unwrap_or_default(),
+              Some(container::StatsOptions {
+                stream: false,
+                ..Default::default()
+              }),
+            )
+            .take(1)
+            .next()
+            .map(|stats| match stats {
+              Some(Ok(stats)) => Some(stats),
+              _ => None,
             })
-            .flatten()
-            .collect::<Vec<_>>()
-        })
-      })
-      .collect();
+            .await,
+        );
 
-    future::join_all(tasks)
-      .await
-      .into_iter()
-      .flat_map(|task| match task {
-        Ok(task) => task,
-        _ => Default::default(),
-      })
-      .collect()
+        task::spawn(Self::new_state_metric(
+          running,
+          Arc::clone(&name),
+          Arc::clone(&stats),
+          Arc::clone(&tx),
+        ));
+
+        task::spawn(Self::new_cpu_metric(
+          running,
+          Arc::clone(&name),
+          Arc::clone(&stats),
+          Arc::clone(&tx),
+        ));
+
+        task::spawn(Self::new_memory_metric(
+          running,
+          Arc::clone(&name),
+          Arc::clone(&stats),
+          Arc::clone(&tx),
+        ));
+
+        task::spawn(Self::new_io_metric(
+          running,
+          Arc::clone(&name),
+          Arc::clone(&stats),
+          Arc::clone(&tx),
+        ));
+
+        task::spawn(Self::new_network_metric(
+          running,
+          Arc::clone(&name),
+          Arc::clone(&stats),
+          Arc::clone(&tx),
+        ));
+      });
+    }
   }
 }
 
 impl DockerCollector {
-  pub fn new_state_metric(
+  pub async fn new_state_metric(
+    running: bool,
     name: Arc<Option<String>>,
     stats: Arc<Option<container::Stats>>,
-    running: bool,
-  ) -> Option<Vec<DockerMetric>> {
-    match (name.as_ref(), stats.as_ref()) {
-      (Some(name), _) => {
-        let metric = family::Family::<DockerMetricLabels, gauge::Gauge>::default();
+    tx: Arc<mpsc::Sender<DockerMetric>>,
+  ) {
+    if let (_, Some(name), _) = (running, name.as_ref(), stats.as_ref()) {
+      let metric = family::Family::<DockerMetricLabels, gauge::Gauge>::default();
 
-        metric
-          .get_or_create(&DockerMetricLabels {
-            container_name: name.to_string(),
-          })
-          .set(running as i64);
+      metric
+        .get_or_create(&DockerMetricLabels {
+          container_name: name.to_string(),
+        })
+        .set(running as i64);
 
-        Some(Vec::from([DockerMetric {
-          name: String::from("container_running"),
-          help: String::from("container running (1 = running, 0 = other)"),
-          unit: None,
-          metric: Box::new(metric),
-        }]))
-      }
-      _ => None,
+      tx.send(DockerMetric {
+        name: String::from("container_running"),
+        help: String::from("container running (1 = running, 0 = other)"),
+        unit: None,
+        metric: Box::new(metric),
+      })
+      .await
+      .unwrap_or_default();
     }
   }
 
-  pub fn new_cpu_metric(
+  pub async fn new_cpu_metric(
+    running: bool,
     name: Arc<Option<String>>,
     stats: Arc<Option<container::Stats>>,
-    running: bool,
-  ) -> Option<Vec<DockerMetric>> {
-    match (name.as_ref(), stats.as_ref(), running) {
-      (Some(name), Some(stats), true) => {
-        let cpu_delta =
-          stats.cpu_stats.cpu_usage.total_usage - stats.precpu_stats.cpu_usage.total_usage;
+    tx: Arc<mpsc::Sender<DockerMetric>>,
+  ) {
+    if let (true, Some(name), Some(stats)) = (running, name.as_ref(), stats.as_ref()) {
+      let cpu_delta =
+        stats.cpu_stats.cpu_usage.total_usage - stats.precpu_stats.cpu_usage.total_usage;
 
-        let system_cpu_delta =
-          stats.cpu_stats.system_cpu_usage? - stats.precpu_stats.system_cpu_usage?;
+      let system_cpu_delta = match (
+        stats.cpu_stats.system_cpu_usage,
+        stats.precpu_stats.system_cpu_usage,
+      ) {
+        (Some(cpu_usage), Some(precpu_usage)) => Some(cpu_usage - precpu_usage),
+        _ => None,
+      };
 
-        let number_cpus = stats.cpu_stats.online_cpus.unwrap_or(1);
+      let number_cpus = stats.cpu_stats.online_cpus.unwrap_or(1);
 
+      if let Some(system_cpu_delta) = system_cpu_delta {
         let cpu_utilization =
           (cpu_delta as f64 / system_cpu_delta as f64) * number_cpus as f64 * 100.0;
 
@@ -170,55 +176,64 @@ impl DockerCollector {
           })
           .set(cpu_utilization);
 
-        Some(Vec::from([DockerMetric {
+        tx.send(DockerMetric {
           name: String::from("cpu_utilization_percent"),
           help: String::from("cpu utilization in percent"),
           unit: None,
           metric: Box::new(metric),
-        }]))
+        })
+        .await
+        .unwrap_or_default();
       }
-
-      _ => None,
     }
   }
 
-  pub fn new_memory_metric(
+  pub async fn new_memory_metric(
+    running: bool,
     name: Arc<Option<String>>,
     stats: Arc<Option<container::Stats>>,
-    running: bool,
-  ) -> Option<Vec<DockerMetric>> {
-    Some(Vec::from([DockerMetric {
+    tx: Arc<mpsc::Sender<DockerMetric>>,
+  ) {
+    tx.send(DockerMetric {
       name: String::from("todo"),
       help: String::from("todo"),
       unit: None,
       metric: Box::new(family::Family::<DockerMetricLabels, gauge::Gauge>::default()),
-    }]))
+    })
+    .await
+    .unwrap_or_default();
   }
 
-  pub fn new_io_metric(
+  pub async fn new_io_metric(
+    running: bool,
     name: Arc<Option<String>>,
     stats: Arc<Option<container::Stats>>,
-    running: bool,
-  ) -> Option<Vec<DockerMetric>> {
-    Some(Vec::from([DockerMetric {
+    tx: Arc<mpsc::Sender<DockerMetric>>,
+  ) {
+    tx.send(DockerMetric {
       name: String::from("todo"),
       help: String::from("todo"),
       unit: None,
       metric: Box::new(family::Family::<DockerMetricLabels, gauge::Gauge>::default()),
-    }]))
+    })
+    .await
+    .unwrap_or_default();
   }
 
-  pub fn new_network_metric(
+  pub async fn new_network_metric(
+    running: bool,
     name: Arc<Option<String>>,
     stats: Arc<Option<container::Stats>>,
-    running: bool,
-  ) -> Option<Vec<DockerMetric>> {
-    Some(Vec::from([DockerMetric {
+    tx: Arc<mpsc::Sender<DockerMetric>>,
+  ) {
+    tx.send(DockerMetric {
       name: String::from("todo"),
       help: String::from("todo"),
       unit: None,
       metric: Box::new(family::Family::<DockerMetricLabels, gauge::Gauge>::default()),
-    }]))
+    })
+    .await
+    .unwrap_or_default();
   }
 }
 
@@ -237,16 +252,16 @@ impl collector::Collector for DockerCollector {
           }
         };
 
-        let metrics = self.collect(docker).await;
+        let (tx, mut rx) = mpsc::channel::<DockerMetric>(32);
+        self.collect(docker, Arc::new(tx)).await;
 
-        for metric in metrics {
-          let DockerMetric {
-            name,
-            help,
-            unit,
-            metric,
-          } = metric;
-
+        while let Some(DockerMetric {
+          name,
+          help,
+          unit,
+          metric,
+        }) = rx.recv().await
+        {
           encoder
             .encode_descriptor(&name, &help, unit.as_ref(), metric.metric_type())
             .and_then(|encoder| metric.encode(encoder))
