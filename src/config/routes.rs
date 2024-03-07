705f17c8a5803932bd695f18e6dcad235f82af09
patrickap <patrick.schlageter@web.de>
2024-03-07 15:01:22 +0100

    feat: improve tests

--- src/config/routes.rs
+++ src/config/routes.rs
@@ -20,17 +20,6 @@ mod tests {
   use prometheus_client::{collector::Collector, encoding::DescriptorEncoder};
   use std::fmt::Error;
 
-  #[derive(Debug)]
-  struct MockCollector {
-    result: Result<(), Error>,
-  }
-
-  impl Collector for MockCollector {
-    fn encode(&self, _: DescriptorEncoder) -> Result<(), Error> {
-      self.result
-    }
-  }
-
   #[tokio::test]
   async fn it_returns_status() {
     let status = status().await;
@@ -39,9 +28,7 @@ mod tests {
 
   #[tokio::test]
   async fn it_returns_metrics_ok() {
-    let mut registry = Registry::from(Default::default());
-    registry.register_collector(Box::new(MockCollector { result: Ok(()) }));
-
+    let registry = Registry::from(Default::default());
     let metrics = metrics(Extension(Arc::new(registry))).await;
 
     assert_eq!(metrics.is_ok(), true);
@@ -50,6 +37,17 @@ mod tests {
 
   #[tokio::test]
   async fn it_returns_metrics_err() {
+    #[derive(Debug)]
+    struct MockCollector {
+      result: Result<(), Error>,
+    }
+
+    impl Collector for MockCollector {
+      fn encode(&self, _: DescriptorEncoder) -> Result<(), Error> {
+        self.result
+      }
+    }
+
     let mut registry = Registry::from(Default::default());
     registry.register_collector(Box::new(MockCollector {
       result: Err(Default::default()),
