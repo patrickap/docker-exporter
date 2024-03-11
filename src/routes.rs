6eedca1754fc0e9ae914b0ddad827418786f27e6
patrickap <patrick.schlageter@web.de>
2024-03-11 14:46:47 +0100

    feat: added tests

--- src/routes.rs
+++ src/routes.rs
@@ -25,44 +25,62 @@ pub async fn metrics(
   }
 }
 
-// #[cfg(test)]
-// mod tests {
-//   use super::*;
-//   use prometheus_client::{collector::Collector, encoding::DescriptorEncoder};
-//   use std::fmt::Error;
+#[cfg(test)]
+mod tests {
+  use std::fmt::Error;
 
-//   #[tokio::test]
-//   async fn it_returns_status() {
-//     let status = status().await;
-//     assert_eq!(status, "ok");
-//   }
+  use prometheus_client::{collector::Collector, encoding::DescriptorEncoder};
 
-//   #[tokio::test]
-//   async fn it_returns_metrics_ok() {
-//     let registry = Registry::from(Default::default());
-//     let metrics = metrics(Extension(Arc::new(registry))).await;
+  use super::*;
 
-//     assert_eq!(metrics.is_ok(), true);
-//     assert_eq!(metrics, Ok(String::from("# EOF\n")));
-//   }
+  #[tokio::test]
+  async fn it_returns_status() {
+    let result = status().await;
+    assert_eq!(result, "ok");
+  }
 
-//   #[tokio::test]
-//   async fn it_returns_metrics_err() {
-//     #[derive(Debug)]
-//     struct MockCollector {}
+  #[tokio::test]
+  async fn it_returns_metrics_ok() {
+    let d = Docker::connect_with_socket_defaults().unwrap();
+    let r = Registry::from(Default::default());
+    let m = Metrics::new();
 
-//     impl Collector for MockCollector {
-//       fn encode(&self, _: DescriptorEncoder) -> Result<(), Error> {
-//         Err(Default::default())
-//       }
-//     }
+    let result = metrics(
+      Extension(Arc::new(d)),
+      Extension(Arc::new(r)),
+      Extension(Arc::new(m)),
+    )
+    .await;
 
-//     let mut registry = Registry::from(Default::default());
-//     registry.register_collector(Box::new(MockCollector {}));
+    assert_eq!(result.is_ok(), true);
+    assert_eq!(result, Ok(String::from("# EOF\n")));
+  }
 
-//     let metrics = metrics(Extension(Arc::new(registry))).await;
+  #[tokio::test]
+  async fn it_returns_metrics_err() {
+    #[derive(Debug)]
+    struct ErrCollector {}
 
-//     assert_eq!(metrics.is_err(), true);
-//     assert_eq!(metrics, Err(StatusCode::INTERNAL_SERVER_ERROR));
-//   }
-// }
+    impl Collector for ErrCollector {
+      fn encode(&self, _: DescriptorEncoder) -> Result<(), Error> {
+        Err(Default::default())
+      }
+    }
+
+    let d = Docker::connect_with_socket_defaults().unwrap();
+    let mut r = Registry::from(Default::default());
+    let m = Metrics::new();
+
+    r.register_collector(Box::new(ErrCollector {}));
+
+    let result = metrics(
+      Extension(Arc::new(d)),
+      Extension(Arc::new(r)),
+      Extension(Arc::new(m)),
+    )
+    .await;
+
+    assert_eq!(result.is_err(), true);
+    assert_eq!(result, Err(StatusCode::INTERNAL_SERVER_ERROR));
+  }
+}
