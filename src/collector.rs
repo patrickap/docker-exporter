b8543904c58e4991e73623f96b3e83ed8ec336de
patrickap <patrick.schlageter@web.de>
2024-12-26 23:57:50 +0100

    chore: test

--- src/collector.rs
+++ src/collector.rs
@@ -4,6 +4,7 @@ use prometheus_client::{
   encoding::{DescriptorEncoder, EncodeMetric},
   metrics::counter::ConstCounter,
 };
+use tokio::{runtime::Handle, task};
 
 #[derive(Debug)]
 pub struct DockerCollector {
@@ -14,12 +15,23 @@ impl DockerCollector {
   pub fn new(docker: Docker) -> Self {
     return Self { docker };
   }
+
+  async fn collect(&self) -> Result<(), Box<dyn std::error::Error>> {
+    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+    Ok(())
+  }
 }
 
 impl Collector for DockerCollector {
   fn encode(&self, mut encoder: DescriptorEncoder) -> Result<(), std::fmt::Error> {
-    let counter = ConstCounter::new(42);
+    let metrics = task::block_in_place(move || {
+      Handle::current()
+        .block_on(async move { self.collect().await })
+        // TODO: do not unwrap
+        .unwrap()
+    });
 
+    let counter = ConstCounter::new(42);
     let metric_encoder =
       encoder.encode_descriptor("my_counter", "some help", None, counter.metric_type())?;
     counter.encode(metric_encoder)?;
