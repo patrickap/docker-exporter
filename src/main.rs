5fadfc30d040eb0fa2d1fce7b5ff8997ecac764f
patrickap <patrick.schlageter@web.de>
2024-12-26 21:45:57 +0100

    refactor: collector

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,13 @@ use std::{error::Error, sync::Arc};
 use tokio::{net::TcpListener, signal};
 
 use crate::{
-  collector::{DockerCollector, DockerMetrics, Metrics},
+  collector::DockerCollector,
   constant::{PROMETHEUS_REGISTRY_PREFIX, SERVER_ADDRESS},
   extension::DockerExt,
 };
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-  let mut registry = Registry::with_prefix(PROMETHEUS_REGISTRY_PREFIX);
-
   let docker = match Docker::try_connect() {
     Ok(docker) => Ok(docker),
     Err(err) => {
@@ -27,23 +25,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
   }?;
 
-  let collector = DockerCollector::new(Arc::new(docker));
+  let mut registry = Registry::with_prefix(PROMETHEUS_REGISTRY_PREFIX);
 
-  let metrics = DockerMetrics::new();
-  metrics.register(&mut registry);
+  registry.register_collector(Box::new(DockerCollector::new(docker)));
 
   let listener = TcpListener::bind(SERVER_ADDRESS).await?;
+
   let router = Router::new()
     .route("/status", routing::get(route::status))
-    .route(
-      "/metrics",
-      routing::get(route::metrics::<DockerCollector<Docker>, DockerMetrics>),
-    )
-    .layer(Extension(Arc::new(registry)))
-    .layer(Extension(Arc::new(collector)))
-    .layer(Extension(Arc::new(metrics)));
+    .route("/metrics", routing::get(route::metrics))
+    .layer(Extension(Arc::new(registry)));
 
   println!("server listening on {}", listener.local_addr()?);
+
   axum::serve(listener, router)
     .with_graceful_shutdown(async {
       if let Ok(_) = signal::ctrl_c().await {
--- src/route.rs
+++ src/route.rs
@@ -2,149 +2,16 @@ use axum::{http::StatusCode, response::IntoResponse, Extension};
 use prometheus_client::{encoding::text, registry::Registry};
 use std::sync::Arc;
 
-use crate::collector::{Collector, Metrics};
-
 pub async fn status() -> Result<impl IntoResponse, StatusCode> {
   Ok((StatusCode::OK, "ok"))
 }
 
-pub async fn metrics<C, M>(
+pub async fn metrics(
   Extension(registry): Extension<Arc<Registry>>,
-  Extension(collector): Extension<Arc<C>>,
-  Extension(metrics): Extension<Arc<M>>,
-) -> Result<impl IntoResponse, StatusCode>
-where
-  C: Collector,
-  M: Metrics,
-  <M as Metrics>::Input: From<<C as Collector>::Output>,
-{
-  let result = collector.collect().await;
-  metrics.clear();
-  metrics.update(result.into());
-
+) -> Result<impl IntoResponse, StatusCode> {
   let mut buffer = String::new();
   match text::encode(&mut buffer, &registry) {
     Ok(_) => Ok((StatusCode::OK, buffer)),
     _ => Err(StatusCode::INTERNAL_SERVER_ERROR),
-  }
-}
-
-#[cfg(test)]
-mod tests {
-  use axum::body::{self, Body};
-  use prometheus_client::metrics::{counter::Counter, family::Family, gauge::Gauge};
-  use std::{error::Error, sync::atomic::AtomicU64};
-
-  use super::*;
-  use crate::{collector::Metric, extension::RegistryExt};
-
-  async fn read_body_as_str(body: Body) -> Result<String, Box<dyn Error>> {
-    let bytes = body::to_bytes(body, usize::MAX).await?;
-    Ok(String::from_utf8(bytes.to_vec())?)
-  }
-
-  #[tokio::test]
-  async fn it_returns_status() {
-    let result = status().await;
-    assert!(result.is_ok());
-
-    let response = result.unwrap().into_response();
-    assert_eq!(response.status(), StatusCode::OK);
-
-    let body = read_body_as_str(response.into_body()).await.unwrap();
-
-    assert_eq!(body, "ok");
-  }
-
-  #[tokio::test]
-  async fn it_returns_metrics() {
-    let mut registry = Registry::from(Default::default());
-
-    struct MockCollector {}
-
-    impl Collector for MockCollector {
-      type Output = i32;
-      async fn collect(&self) -> Self::Output {
-        123
-      }
-    }
-
-    struct MockMetrics {
-      gauge_metric_test: Metric<Family<Vec<(String, String)>, Gauge<f64, AtomicU64>>>,
-      counter_metric_test: Metric<Family<Vec<(String, String)>, Counter<f64, AtomicU64>>>,
-    }
-
-    impl Metrics for MockMetrics {
-      type Origin = MockCollector;
-      type Input = <Self::Origin as Collector>::Output;
-      fn register(&self, registry: &mut Registry) {
-        registry.register_metric(&self.gauge_metric_test);
-        registry.register_metric(&self.counter_metric_test);
-      }
-      fn clear(&self) {
-        self.gauge_metric_test.metric.clear();
-        self.counter_metric_test.metric.clear();
-      }
-      fn update(&self, input: Self::Input) {
-        self
-          .gauge_metric_test
-          .metric
-          .get_or_create(&Vec::from([(
-            "gauge_label_key_test".to_owned(),
-            "gauge_label_value_test".to_owned(),
-          )]))
-          .set(input as f64);
-        self
-          .counter_metric_test
-          .metric
-          .get_or_create(&Vec::from([(
-            "counter_label_key_test".to_owned(),
-            "counter_label_value_test".to_owned(),
-          )]))
-          .inc();
-      }
-    }
-
-    let collector = MockCollector {};
-
-    let metrics = MockMetrics {
-      gauge_metric_test: Metric::new(
-        "gauge_metric_test",
-        "gauge_metric_help_test",
-        Default::default(),
-      ),
-      counter_metric_test: Metric::new(
-        "counter_metric_test",
-        "counter_metric_help_test",
-        Default::default(),
-      ),
-    };
-
-    metrics.register(&mut registry);
-
-    let result = super::metrics(
-      Extension(Arc::new(registry)),
-      Extension(Arc::new(collector)),
-      Extension(Arc::new(metrics)),
-    )
-    .await;
-
-    assert!(result.is_ok());
-
-    let response = result.unwrap().into_response();
-    assert_eq!(response.status(), StatusCode::OK);
-
-    let body = read_body_as_str(response.into_body()).await.unwrap();
-
-    let expected = "".to_owned()
-      + "# HELP gauge_metric_test gauge_metric_help_test.\n"
-      + "# TYPE gauge_metric_test gauge\n"
-      + "gauge_metric_test{gauge_label_key_test=\"gauge_label_value_test\"} 123.0\n"
-      + "# HELP counter_metric_test counter_metric_help_test.\n"
-      + "# TYPE counter_metric_test counter\n"
-      + "counter_metric_test_total{counter_label_key_test=\"counter_label_value_test\"} 1.0\n"
-      + "# EOF\n";
-
-    assert_eq!(body, expected);
   }
 }
