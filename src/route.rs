0ec16880b5fa3d6b9a99b11ea2488a17f7a52743
patrickap <patrick.schlageter@web.de>
2025-01-02 20:59:44 +0100

    chore: tests

--- src/route.rs
+++ src/route.rs
@@ -15,3 +15,92 @@ pub async fn metrics(
     _ => Err(StatusCode::INTERNAL_SERVER_ERROR),
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use axum::body::{self, Body};
+  use prometheus_client::{
+    collector::Collector,
+    encoding::{DescriptorEncoder, EncodeMetric},
+    metrics::{counter::ConstCounter, gauge::ConstGauge},
+  };
+  use std::error::Error;
+
+  use super::*;
+
+  async fn read_body_as_str(body: Body) -> Result<String, Box<dyn Error>> {
+    let bytes = body::to_bytes(body, usize::MAX).await?;
+    Ok(String::from_utf8(bytes.to_vec())?)
+  }
+
+  #[tokio::test]
+  async fn it_returns_status() {
+    let result = status().await;
+    assert!(result.is_ok());
+
+    let response = result.unwrap().into_response();
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = read_body_as_str(response.into_body()).await.unwrap();
+    assert_eq!(body, "ok");
+  }
+
+  #[tokio::test]
+  async fn it_returns_metrics() {
+    #[derive(Debug)]
+    struct MockCollector {}
+
+    impl Collector for MockCollector {
+      fn encode(&self, mut encoder: DescriptorEncoder) -> Result<(), std::fmt::Error> {
+        {
+          let gauge = ConstGauge::new(1.0 as f64);
+          let gauge_encoder = encoder.encode_descriptor(
+            "const_gauge_name",
+            "const_gauge_help",
+            None,
+            gauge.metric_type(),
+          )?;
+          gauge.encode(gauge_encoder)?;
+        }
+
+        {
+          let counter = ConstCounter::new(1.0 as f64);
+          let counter_encoder = encoder.encode_descriptor(
+            "const_counter_name",
+            "const_counter_help",
+            None,
+            counter.metric_type(),
+          )?;
+          counter.encode(counter_encoder)?;
+        }
+
+        Ok(())
+      }
+    }
+
+    let collector = MockCollector {};
+
+    let mut registry = Registry::default();
+    registry.register_collector(Box::new(collector));
+
+    let result = metrics(Extension(Arc::new(registry))).await;
+
+    assert!(result.is_ok());
+
+    let response = result.unwrap().into_response();
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = read_body_as_str(response.into_body()).await.unwrap();
+
+    let expected = "".to_owned()
+      + "# HELP const_gauge_name const_gauge_help\n"
+      + "# TYPE const_gauge_name gauge\n"
+      + "const_gauge_name 1.0\n"
+      + "# HELP const_counter_name const_counter_help\n"
+      + "# TYPE const_counter_name counter\n"
+      + "const_counter_name_total 1.0\n"
+      + "# EOF\n";
+
+    assert_eq!(body, expected);
+  }
+}
